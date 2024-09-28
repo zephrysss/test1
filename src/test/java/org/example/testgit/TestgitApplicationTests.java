fb9dd59bd2e106bea403c8157ca669d56d3f441d
zephrysss <3020234294@tju.edu.cn>
2024-09-28 10:55:24 +0800

    Init

---
+++ src/test/java/org/example/testgit/TestgitApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.testgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestgitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
